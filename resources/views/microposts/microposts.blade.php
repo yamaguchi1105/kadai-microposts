64ea242cafdb4db43f0d43c8132b7d01a8779084
yamaguchi <weareset1105@gmail.com>
2020-03-10 13:07:22 +0000

    modify

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -9,15 +9,17 @@
                 <div>
                     <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                 </div>
-                <div>
-                    
+                <div class="form-group row">
+                    <div class="col-1.5">
                     @include('user_favorit.favorit_button', ['user' => $user])
-                    
+                    </div>
+                    <div class="col-1.5">
                     @if (Auth::id() == $micropost->user_id)
                         {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                             {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
                         {!! Form::close() !!}
                     @endif
+                    </div>
                 </div>
             </div>
         </li>
